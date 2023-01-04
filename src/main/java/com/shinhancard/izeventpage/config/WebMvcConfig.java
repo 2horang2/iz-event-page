26e4e34ab0e8900c7862a5509cf3c334e8d8398a
2horang2 <cmcm1284@naver.com>
2023-01-04 13:03:23 +0900

    초기 html 템플릿 적용 완료

--- src/main/java/com/shinhancard/izeventpage/config/WebMvcConfig.java
+++ src/main/java/com/shinhancard/izeventpage/config/WebMvcConfig.java
@@ -1,14 +1,16 @@
 package com.shinhancard.izeventpage.config;
 
+import java.util.concurrent.TimeUnit;
+
 import com.shinhancard.izeventpage.interceptor.RequestInterceptor;
 import com.shinhancard.izeventpage.interceptor.ResponseInterceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -17,6 +19,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     RequestInterceptor requestInterceptor;
     @Autowired
     ResponseInterceptor responseInterceptor;
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/", "classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
